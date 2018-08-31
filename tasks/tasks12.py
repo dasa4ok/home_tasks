64405a58d850f94a43955740a6b3e1c4348daf84
dasa4ok <42151955+dasa4ok@users.noreply.github.com>
2018-08-31 19:03:45 +0300

    Update tasks12.py

--- tasks/tasks12.py
+++ tasks/tasks12.py
@@ -1,9 +1,9 @@
 #12. Дан список значений. Превратить список в словарь поочередно превращая елменты в ключи и значения. [1, 2, 3, 4, 5, 6] -> {1: 2, 3: 4, 5: 6}
 def dict_maker(values):
-    dict = {}
+    made_dict = {}
     index = 0
     for value in values[0::2]:
-        dict[value] = values[index + 1]
+        made_dict[value] = values[index + 1]
         index += 2
     return dict
 
